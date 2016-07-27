c0ddcf0dc2107c733e8ffe98caf0ed607c8d07c2
dilipptdr <dilip.ptdr@gmail.com>
2016-07-28 00:43:42 +0530

    Update CustomView.h

--- MaterialDesigniOS/MaterialDesigniOS/CustomView.h
+++ MaterialDesigniOS/MaterialDesigniOS/CustomView.h
@@ -1,17 +1,11 @@
 //
 // CustomViewsView.h
-//  Parallax
-//
-//  Created by Domenico Vacchiano on 01/04/15.
-//  Copyright (c) 2015 LamCube. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
 
-typedef NS_OPTIONS(NSUInteger,CustomViewOptions) {
-   CustomViewOptionsIsSwipeableCard = 1 << 0
-};
+
 
 
 @protocol CustomViewDelegate <NSObject>
